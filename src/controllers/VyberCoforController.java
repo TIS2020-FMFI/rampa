0a5bfae83a06b1e088db71e4c84a269fc3061dd8
barboradurackova <61238619+barboradurackova@users.noreply.github.com>
2021-01-25 15:07:14 +0100

    ukladanie udajov a rozrobene nacitavanie (2 a 3)
    dajte vediet co treba zmenit kamosi B)

---
+++ src/controllers/VyberCoforController.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import main.Main;
+
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
+public class VyberCoforController implements Initializable{
+
+    @FXML
+    private TextArea savedCoforsOutput;
+    @FXML
+    private TextField coforInput;
+
+    private StringBuilder sb = new StringBuilder("");
+    private String path;
+
+    public VyberCoforController() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/fxml/vyberCofor.fxml"));
+        loader.setController(this);
+
+        Main.vyberCoforStage.setScene(new Scene(loader.load()));
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        URL outputt = getClass().getResource("../resources/database/cofors.txt");
+        path = outputt.getPath();
+
+        try {
+            readSavedCofors();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        savedCoforsOutput.appendText(String.valueOf(sb));
+    }
+
+    public void potvrditBtnClick(MouseEvent mouseEvent) throws IOException {
+        Main.vyberCoforStage.hide();
+    }
+
+    public void spatBtnClick(MouseEvent mouseEvent) throws IOException {
+        Main.vyberCoforStage.hide();
+    }
+
+    public void readSavedCofors() throws  IOException{
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        String line;
+
+        while((line = br.readLine())!=null)
+        {
+            line = line.split("\\$")[0];
+            for (String data : line.split(";")) {
+                sb.append(data.split("#")[0]).append(": ");
+                sb.append(data.split("#")[1]).append(", ");
+                }
+            sb.setLength(sb.length() - 2);
+            sb.append("\n");
+            }
+
+        br.close();
+    }
+}
+
+
--- src/main/Main.java
+++ src/main/Main.java
@@ -38,6 +38,7 @@ public class Main extends Application {
     public static Stage statVzdialenostStage = null;
     public static Stage technickeUdajeStage = null;
     public static Stage udajeDodavatelaStage = null;
+    public static Stage vyberCoforStage = null;
     public static Stage ukazkaStage = null;
 
 
@@ -77,6 +78,9 @@ public class Main extends Application {
 
         udajeDodavatelaStage = new Stage();
         udajeDodavatelaStage.setTitle("Pridanie dodávateľa");
+
+        vyberCoforStage = new Stage();
+        vyberCoforStage.setTitle("Vyberanie dodávateľa");
 
         ukazkaStage = new Stage();
         ukazkaStage.setTitle("Ukážka");
